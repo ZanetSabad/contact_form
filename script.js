21a1cd83e9daea3604fba14844ea1a12bdd5d041
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2022-09-08 21:10:23 +0200

    vytvořený message.php a připojení na JS + Zk)

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let form = document.querySelector("form")
-statusTxt = form.querySelector("button span")
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button span");
 
+
+form.onsubmit = (event)=>{
+    event.preventDefault(); //zabránění v odeslání
+    statusTxt.style.display = "block";
+  
+    let xmlObject = new XMLHttpRequest(); //vytvoření xml objectu
+    xmlObject.open("POST", "message.php", true); // žádost do message.php
+    xmlObject.onload = ()=>{    //po načtení ajaxu
+        if(xmlObject.readyState == 4 && xmlObject.status == 200){ 
+            let response = xmlObject.response;   //uložení respons ajax do proměnné
+            console.log(response);
+        }
+    }
+        xmlObject.send();
+    
+}
